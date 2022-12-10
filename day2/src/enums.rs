960f99e256b92cefc7f61075ed66f0b1844a74b8
George Kampanos <30287348+kampanosg@users.noreply.github.com>
2022-12-10 17:04:31 -0500

    Solution: Day 2 (#3)
    * part 2 solution
    
    * refactoring
    
    * updated readme
    
    * formatting
    
    * tests
    
    * start
    
    * basic file parsing and rules
    
    * created hand enum
    
    * parse the two hands
    
    * determine result
    
    * bring everything together for the solution
    
    * added unit test
    
    * finished part 1
    
    * refactored for p2 and added new enum
    
    * more refactoring to make var names better
    
    * art2 solution
    
    * fixed a bug making the count wrong
    
    * added readme file for solution
    
    * tests

---
+++ day2/src/enums.rs
@@ -0,0 +1,60 @@
+#[derive(Debug, Clone, Copy)]
+pub enum Hand {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Debug)]
+pub enum Result {
+    Win,
+    Lose,
+    Draw,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Outcome {
+    Win,
+    Lose,
+    Draw,
+}
+
+impl Hand {
+    pub fn transform(v: &str) -> Option<Hand> {
+        match v {
+            "A" | "X" => Some(Hand::Rock),
+            "B" | "Y" => Some(Hand::Paper),
+            "C" | "Z" => Some(Hand::Scissors),
+            _ => None,
+        }
+    }
+
+    pub fn bonus_points(&self) -> u64 {
+        match *self {
+            Hand::Rock => 1,
+            Hand::Paper => 2,
+            Hand::Scissors => 3,
+        }
+    }
+}
+
+impl Result {
+    pub fn points(&self) -> u64 {
+        match *self {
+            Result::Win => 6,
+            Result::Draw => 3,
+            Result::Lose => 0,
+        }
+    }
+}
+
+impl Outcome {
+    pub fn transform(v: &str) -> Option<Outcome> {
+        match v {
+            "X" => Some(Outcome::Lose),
+            "Y" => Some(Outcome::Draw),
+            "Z" => Some(Outcome::Win),
+            _ => None,
+        }
+    }
+}
---
+++ day2/src/main.rs
@@ -0,0 +1,155 @@
+pub mod enums;
+use std::{env, fs};
+
+// Rules:
+//
+// Points: Loss = 0, Draw = 3, Win = 6
+// Points for hand: Rock = 1, Paper = 2, Scissors = 3
+//
+// p1 = Rock = A, Paper = B, Scissors = C
+// Part 1: Rock = X, Paper = Y, Scissors = Z
+// Part 2: X = Lose, Y = Draw, Z = Win
+
+fn main() {
+    let file_path = env::args().nth(1).expect("param not provided: file_path");
+    let part = env::args()
+        .nth(2)
+        .expect("param not provided: part (valid options: p1 | p2)");
+
+    let file_contents = fs::read_to_string(file_path).unwrap();
+
+    let rounds = file_contents
+        .trim_end_matches("\n")
+        .split("\n")
+        .collect::<Vec<&str>>();
+
+    match part.as_str() {
+        "p1" => p1(rounds),
+        "p2" => p2(rounds),
+        _ => println!("invalid part param (valid options: p1 | p2)"),
+    }
+}
+
+fn p1(rounds: Vec<&str>) {
+    let mut total_points: u64 = 0;
+    for round in rounds.iter() {
+        let (opponent_hand, player_hand) = transform_round_p1(round);
+        total_points += calc_round_points(opponent_hand, player_hand);
+    }
+    println!("total points: {}", total_points);
+}
+
+fn p2(rounds: Vec<&str>) {
+    let mut total_points: u64 = 0;
+    for round in rounds.iter() {
+        let (opponent_hand, desired_outcome) = transform_round_p2(round);
+        let player_hand = determine_hand(opponent_hand, desired_outcome);
+        total_points += calc_round_points(opponent_hand, player_hand);
+    }
+    println!("total points: {}", total_points);
+}
+
+fn calc_round_points(hand1: enums::Hand, hand2: enums::Hand) -> u64 {
+    let result = determine_result(hand1, hand2);
+    result.points() + hand2.bonus_points()
+}
+
+fn transform_round_p1(round: &str) -> (enums::Hand, enums::Hand) {
+    let hands: Vec<&str> = round.trim_end_matches("\n").split(" ").collect();
+
+    let opponent_hand = enums::Hand::transform(hands[1]).expect("malformed input");
+    let player_hand = enums::Hand::transform(hands[1]).expect("malformed input");
+
+    (opponent_hand, player_hand)
+}
+
+fn transform_round_p2(round: &str) -> (enums::Hand, enums::Outcome) {
+    let round: Vec<&str> = round.trim_end_matches("\n").split(" ").collect();
+
+    let opponent_hand = enums::Hand::transform(round[0]).expect("malformed input");
+    let desired_outcome = enums::Outcome::transform(round[1]).expect("malformed input");
+
+    (opponent_hand, desired_outcome)
+}
+
+fn determine_result(hand1: enums::Hand, hand2: enums::Hand) -> enums::Result {
+    match hand2 {
+        enums::Hand::Rock => match hand1 {
+            enums::Hand::Rock => enums::Result::Draw,
+            enums::Hand::Paper => enums::Result::Lose,
+            enums::Hand::Scissors => enums::Result::Win,
+        },
+        enums::Hand::Paper => match hand1 {
+            enums::Hand::Rock => enums::Result::Win,
+            enums::Hand::Paper => enums::Result::Draw,
+            enums::Hand::Scissors => enums::Result::Lose,
+        },
+        enums::Hand::Scissors => match hand1 {
+            enums::Hand::Rock => enums::Result::Lose,
+            enums::Hand::Paper => enums::Result::Win,
+            enums::Hand::Scissors => enums::Result::Draw,
+        },
+    }
+}
+
+fn determine_hand(opponent_hand: enums::Hand, desired_outcome: enums::Outcome) -> enums::Hand {
+    match desired_outcome {
+        enums::Outcome::Win => match opponent_hand {
+            enums::Hand::Rock => enums::Hand::Paper,
+            enums::Hand::Paper => enums::Hand::Scissors,
+            enums::Hand::Scissors => enums::Hand::Rock,
+        },
+        enums::Outcome::Draw => match opponent_hand {
+            enums::Hand::Rock => enums::Hand::Rock,
+            enums::Hand::Paper => enums::Hand::Paper,
+            enums::Hand::Scissors => enums::Hand::Scissors,
+        },
+        enums::Outcome::Lose => match opponent_hand {
+            enums::Hand::Rock => enums::Hand::Scissors,
+            enums::Hand::Paper => enums::Hand::Rock,
+            enums::Hand::Scissors => enums::Hand::Paper,
+        },
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{calc_round_points, enums};
+
+    const ROCK: enums::Hand = enums::Hand::Rock;
+    const PAPER: enums::Hand = enums::Hand::Paper;
+    const SCISSORS: enums::Hand = enums::Hand::Scissors;
+
+    #[test]
+    pub fn test_calc_round_points_winner() {
+        let res1 = calc_round_points(ROCK, PAPER);
+        let res2 = calc_round_points(PAPER, SCISSORS);
+        let res3 = calc_round_points(SCISSORS, ROCK);
+
+        assert_eq!(res1, 8);
+        assert_eq!(res2, 9);
+        assert_eq!(res3, 7);
+    }
+
+    #[test]
+    pub fn test_calc_round_points_loser() {
+        let res1 = calc_round_points(ROCK, SCISSORS);
+        let res2 = calc_round_points(PAPER, ROCK);
+        let res3 = calc_round_points(SCISSORS, PAPER);
+
+        assert_eq!(res1, 3);
+        assert_eq!(res2, 1);
+        assert_eq!(res3, 2);
+    }
+
+    #[test]
+    pub fn test_calc_round_points_draw() {
+        let res1 = calc_round_points(ROCK, ROCK);
+        let res2 = calc_round_points(PAPER, PAPER);
+        let res3 = calc_round_points(SCISSORS, SCISSORS);
+
+        assert_eq!(res1, 4);
+        assert_eq!(res2, 5);
+        assert_eq!(res3, 6);
+    }
+}
