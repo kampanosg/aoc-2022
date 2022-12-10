759001f96be6bd33b914c6b7eb4333bc5f98ac7d
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-10 11:05:31 +0000

    a bit cleaner

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,28 +5,25 @@ fn main() {
     let file_path = env::args().nth(1)
         .expect("param not provided");
 
-    let contents = fs::read_to_string(file_path)
-        .expect("unable to open file provided");
+    let file_contents = fs::read_to_string(file_path).unwrap();
 
-    let split = contents.split("\n");
-    let vec: Vec<&str> = split.collect();
+    let elf_cals: Vec<&str> = file_contents.split("\n\n").collect();
 
-    let mut max = 0;
-    let mut current = 0;
+    let mut highest_cals = 0;
 
-    for num in vec {
-        match num {
-            "" => {
-                if current > max {
-                    max = current;
-                }
-                current = 0;
-            },
-            _ => {
-                let calories: i32 = num.parse().unwrap();
-                current += calories;
-            }
-        }
+    for elf in elf_cals {
+        let elf = elf.trim_end_matches("\n");
+        let elf_cals = elf
+            .split("\n")
+            .map(|e| e.parse().unwrap())
+            .collect::<Vec<i64>>()
+            .iter()
+            .sum::<i64>();
+
+       if elf_cals > highest_cals {
+            highest_cals = elf_cals
+       }
     }
-    println!("max: {}", max);
+    println!("highest cals: {}", highest_cals);
 }
+
