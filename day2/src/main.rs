875becfa2809cb315b9792db769a82227bd5c35d
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-10 14:15:30 +0000

    basic file parsing and rules

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,26 @@
+use std::{env, fs};
+
+
+// Rules:
+//
+// Points: Loss = 0, Draw = 3, Win = 6
+// Points for hand: Rock = 1, Paper = 2, Scissors = 3
+//
+// p1 = Rock = A, Paper = B, Scissors = C
+// p2 = Rock = X, Paper = Y, Scissors = Z
+
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = env::args().nth(1).expect("param not provided: file_path");
+    let file_contents = fs::read_to_string(file_path).unwrap();
+    
+    let rounds: Vec<&str> = file_contents
+        .trim_end_matches("\n")
+        .split("\n")
+        .collect();
+    
+    for round in rounds.iter() {
+        println!("{}", round);
+        break;
+    }
 }
