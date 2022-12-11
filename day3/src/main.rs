5267a8e16792f475ea7504666750972abed5ee65
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-11 18:15:29 +0000

    parsed file into a vector of rucksacks

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,17 @@
-use std::env;
+use std::{env, fs};
 
+#[derive(Debug)]
+struct Rucksack {
+    name: String
+}
+
+impl Rucksack {
+    
+    fn new(val: &str) -> Rucksack {
+        return Rucksack { name: String::from(val) }
+    }
+
+}
 
 fn main() {
 
@@ -8,6 +20,18 @@ fn main() {
 
     let part = env::args().nth(2)
         .expect("param not provided: part");
+
+
+    let file_contents = fs::read_to_string(file_path).unwrap();
+    let rucksacks = file_contents
+        .trim_end_matches("\n")
+        .split("\n")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|r| Rucksack::new(r))
+        .collect::<Vec<Rucksack>>();
+
+    println!("{:?}", rucksacks);
 
     match part.as_str() {
         "p1" => p1(),
