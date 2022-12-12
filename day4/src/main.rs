47e36174187a20bf17c47e0e51bcb85a37e5d555
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-12 12:26:19 +0000

    part 2 with tests

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 
     match part.as_str() {
         "p1" => p1(assignments),
+        "p2" => p2(assignments),
         _ => println!("invalid part param (valid options: p1 | p2)"),
     }
 }
@@ -25,7 +26,20 @@ fn p1(assignments: Vec<&str>) {
         if is_exactly_contained(range1, range2) {
             total += 1;
         }
-        break;
+    }
+    println!("{}", total);
+}
+
+
+fn p2(assignments: Vec<&str>) {
+    let mut total = 0;
+    for assignment in assignments {
+        let pair = assignment.split(",").collect::<Vec<&str>>();
+        let range1 = gen_range(pair[0]);
+        let range2 = gen_range(pair[1]);
+        if is_overlapping(range1, range2) {
+            total += 1;
+        }
     }
     println!("{}", total);
 }
@@ -56,9 +70,22 @@ fn is_contained(smaller: Vec<u64>, larger: Vec<u64>) -> bool {
     sm_fst >= lg_fst && sm_lst <= lg_lst
 }
 
+fn is_overlapping(range1: Vec<u64>, range2: Vec<u64>) -> bool {
+    if range1.len() > range2.len() {
+        return overlaps(range2, range1);
+    }
+    overlaps(range1, range2)
+}
+
+fn overlaps(smaller: Vec<u64>, larger: Vec<u64>) -> bool {
+    let (sm_fst, sm_lst) = (smaller[0], smaller[smaller.len() - 1]);
+    let (lg_fst, lg_lst) = (larger[0], larger[larger.len() - 1]);
+    (sm_fst >= lg_fst && sm_fst <= lg_lst) || (sm_lst >= lg_fst && sm_lst <= lg_lst)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{parse_pair, gen_range, is_exactly_contained};
+    use crate::{parse_pair, gen_range, is_exactly_contained, is_overlapping};
 
     #[test]
     fn test_parse_pair() {
@@ -98,6 +125,32 @@ mod tests {
         assert!(!is_exactly_contained(vec![5, 6, 7], vec![7, 8, 9]));
         assert!(!is_exactly_contained(vec![5, 6, 7], vec![7, 8, 9]));
         assert!(!is_exactly_contained(vec![2, 3, 4, 5, 6], vec![4, 5, 6, 7, 8]));
-
     }
+
+    #[test]
+    fn test_is_overlapping() {
+        assert!(is_overlapping(vec![1], vec![1]));
+        assert!(is_overlapping(vec![1], vec![1, 2]));
+        assert!(is_overlapping(vec![2], vec![1, 2]));
+        assert!(is_overlapping(vec![1, 2], vec![1]));
+        assert!(is_overlapping(vec![1, 2], vec![2]));
+        assert!(is_overlapping(vec![1, 2], vec![2, 3]));
+        assert!(is_overlapping(vec![2, 3], vec![1, 2]));
+
+        assert!(!is_overlapping(vec![1], vec![2]));
+        assert!(!is_overlapping(vec![2], vec![1]));
+        assert!(!is_overlapping(vec![1, 2, 3], vec![4, 5, 6]));
+        assert!(!is_overlapping(vec![4, 5, 6], vec![1, 2, 3]));
+
+        // examples from problem definition
+        assert!(is_overlapping(vec![2, 3, 4, 5, 6, 7, 8], vec![3, 4, 5, 6, 7]));
+        assert!(is_overlapping(vec![6], vec![4, 5, 6]));
+        assert!(!is_overlapping(vec![2, 3, 4], vec![6, 7, 8]));
+        assert!(is_overlapping(vec![3, 4], vec![4, 5]));
+        assert!(is_overlapping(vec![5, 6, 7], vec![7, 8, 9]));
+        assert!(is_overlapping(vec![5, 6, 7], vec![7, 8, 9]));
+        assert!(is_overlapping(vec![5, 6, 7], vec![7, 8, 9]));
+        assert!(is_overlapping(vec![2, 3, 4, 5, 6], vec![4, 5, 6, 7, 8]));
+    }
+
 }
