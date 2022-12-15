bc84149bd325a2694c63178e5004b533a2592554
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-15 23:20:29 +0000

    refactor

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,7 +1,34 @@
 pub mod structs;
 use std::{collections::HashMap, env, fs};
+use id_tree::*;
+use id_tree::InsertBehavior::*;
 
 fn main() {
+
+
+//     let mut tree: Tree<i32> = TreeBuilder::new()
+//         .with_node_capacity(5)
+//         .build();
+
+//     let root_id: NodeId = tree.insert(Node::new(0), AsRoot).unwrap();
+//     let child_id: NodeId = tree.insert(Node::new(1), UnderNode(&root_id)).unwrap();
+//     tree.insert(Node::new(2), UnderNode(&root_id)).unwrap();
+//     tree.insert(Node::new(3), UnderNode(&child_id)).unwrap();
+//     tree.insert(Node::new(4), UnderNode(&child_id)).unwrap();
+// tree.insert(Node::new(5), UnderNode(&child_id)).unwrap();
+// tree.insert(Node::new(6), UnderNode(&child_id)).unwrap();
+
+// let mut s = String::new();
+// tree.write_formatted(&mut s).unwrap();
+// println!("{}", s);
+
+
+    // println!("Pre-order:");
+    // for node in tree.traverse_pre_order(&root_id).unwrap() {
+    //     print!("{}, ", node.data());
+    // }
+
+
     let file_path = env::args().nth(1).expect("param not provided: file_path");
     let part = env::args()
         .nth(2)
@@ -21,27 +48,40 @@ fn main() {
 }
 
 fn p1(commands: Vec<&str>) {
-    let mut current_dir = structs::Directory::new(String::from("/"));
 
-    for c in &commands[1..] {
-        let command = c.split(" ").collect::<Vec<&str>>();
+    let mut tree: Tree<structs::Directory> = TreeBuilder::new()
+        .with_node_capacity(5)
+        .build();
 
-        println!("{:?}", command);
-        match command[0] {
-            "$" => {
-                if command[1] == "cd" {
-                    if command[1] == ".." {
-                    } else {
-                    }
-                }
-            }
-            "dir" => {
-                let dir_name = command[1];
-                current_dir.append_dir(dir_name);
-            }
-            _ => {}
-        }
-    }
+    let root = structs::Directory::new("/".to_string(), None);
+    let current_dir: NodeId = tree.insert(Node::new(root), AsRoot).unwrap();
 
-    println!("{:?}", current_dir);
+    // for c in &commands[1..] {
+    //     let command = c.split(" ").collect::<Vec<&str>>();
+
+    //     println!("{:?}", command);
+    //     match command[0] {
+    //         "$" => {
+    //             if command[1] == "cd" {
+    //                 if command[1] == ".." {
+    //                 } else {
+    //                     let dir_name = command[2];
+    //                     let next_dir = current_dir.get_dir(dir_name);
+    //                     current_dir = next_dir.clone();
+    //                 }
+    //             }
+    //         }
+    //         "dir" => {
+    //             let dir_name = command[1];
+    //             current_dir.append_dir(dir_name);
+    //         }
+    //         _ => {
+    //             let file_name = command[1];
+    //             let file_size = command[0].parse::<u64>().unwrap();
+    //             current_dir.append_file(file_name, file_size);
+    //         }
+    //     }
+    // }
+
+    // println!("{:?}", current_dir);
 }
--- day7/src/structs.rs
+++ day7/src/structs.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use id_tree::NodeId;
+
 #[derive(Debug, Clone)]
 pub struct File {
     name: String,
@@ -8,21 +10,18 @@ pub struct File {
 
 #[derive(Debug, Clone)]
 pub struct Directory {
-    name: String,
-    dirs: HashMap<String, Directory>,
-    files: Vec<File>,
+    pub name: String,
+    pub files: HashMap<String, File>,
+    pub parrent: Option<NodeId>,
 }
 
 impl Directory {
-    pub fn new(name: String) -> Directory {
-        Directory {
-            name: name,
-            dirs: HashMap::new(),
-            files: vec![],
-        }
-    }
+    pub fn new(name: String, parrent: Option<NodeId>) -> Self { Self { name, files: HashMap::new(), parrent } }
 
-    pub fn append_dir(&mut self, name: &str) {
-        self.dirs.entry(name.to_string()).or_insert(Directory::new(name.to_string()));
+    pub fn append_file(&mut self, name: &str, size: u64) {
+        self.files.entry(name.to_string()).or_insert(File {
+            name: name.to_string(),
+            size,
+        });
     }
 }
