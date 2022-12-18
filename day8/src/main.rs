d6a7315f768fa3d31381c0b07353bf7fe5c6c5d1
George Kampanos <30287348+kampanosg@users.noreply.github.com>
2022-12-18 10:17:16 -0500

    Solution: Day 8 (#9)
    * project setup
    
    * forrest structure and tests
    
    * p1 solution
    
    * remove comments
    
    * solution to p2
    
    * refactoring

---
+++ day8/src/main.rs
@@ -0,0 +1,222 @@
+pub mod structs;
+use std::{env, fs};
+
+fn main() {
+    let file_path = env::args().nth(1).expect("param not provided: file_path");
+    let part = env::args().nth(2).expect("param not provided: path");
+
+    let file_contents = fs::read_to_string(file_path).unwrap();
+    let forrest = parse_forrest(file_contents);
+    match part.as_str() {
+        "p1" => p1(forrest),
+        "p2" => p2(forrest),
+        _ => println!(""),
+    }
+}
+
+fn p1(forrest: structs::Forrest) {
+    let mut visible_trees = 0;
+    for tree_row in forrest.trees.clone() {
+        for tree in tree_row {
+            if forrest.is_edge(tree.clone()) {
+                visible_trees += 1;
+                continue;
+            }
+
+            // top to bottom
+            let mut vertical_index = 0;
+            let mut horizontal_index = tree.coords.x;
+            let mut found_bigger_tree = false;
+
+            while vertical_index != tree.coords.y {
+                let next_tree =
+                    forrest.trees[vertical_index as usize][horizontal_index as usize].clone();
+                if next_tree.height >= tree.height {
+                    found_bigger_tree = true;
+                    break;
+                }
+                vertical_index += 1;
+            }
+
+            if !found_bigger_tree {
+                visible_trees += 1;
+                continue;
+            }
+
+            // bottom to top
+            found_bigger_tree = false;
+            vertical_index = forrest.edge.y;
+            horizontal_index = tree.coords.x;
+            while vertical_index != tree.coords.y {
+                let next_tree =
+                    forrest.trees[vertical_index as usize][horizontal_index as usize].clone();
+                if next_tree.height >= tree.height {
+                    found_bigger_tree = true;
+                    break;
+                }
+                vertical_index -= 1;
+            }
+
+            if !found_bigger_tree {
+                visible_trees += 1;
+                continue;
+            }
+
+            // left to right
+            found_bigger_tree = false;
+            vertical_index = tree.coords.y;
+            horizontal_index = 0;
+            while horizontal_index != tree.coords.x {
+                let next_tree =
+                    forrest.trees[vertical_index as usize][horizontal_index as usize].clone();
+                if next_tree.height >= tree.height {
+                    found_bigger_tree = true;
+                    break;
+                }
+                horizontal_index += 1;
+            }
+
+            if !found_bigger_tree {
+                visible_trees += 1;
+                continue;
+            }
+
+            // right to left
+            found_bigger_tree = false;
+            vertical_index = tree.coords.y;
+            horizontal_index = forrest.edge.x;
+            while horizontal_index != tree.coords.x {
+                let next_tree =
+                    forrest.trees[vertical_index as usize][horizontal_index as usize].clone();
+                if next_tree.height >= tree.height {
+                    found_bigger_tree = true;
+                    break;
+                }
+                horizontal_index -= 1;
+            }
+
+            if !found_bigger_tree {
+                visible_trees += 1;
+                continue;
+            }
+        }
+    }
+    println!("visible trees: {:?}", visible_trees);
+}
+
+fn p2(forrest: structs::Forrest) {
+    let mut highest_visibility = 0;
+    for tree_row in forrest.trees.clone() {
+        for tree in tree_row {
+            if forrest.is_edge(tree.clone()) {
+                continue;
+            }
+
+            let mut vertical_index = tree.coords.y - 1;
+            let mut horizontal_index = tree.coords.x;
+
+            let mut looking_up_visibile_trees = 0;
+            let mut looking_down_visible_tress = 0;
+            let mut looking_left_visible_trees = 0;
+            let mut looking_right_visible_trees = 0;
+
+            // look up
+            while vertical_index != -1 {
+                let next_tree =
+                    forrest.trees[vertical_index as usize][horizontal_index as usize].clone();
+                looking_up_visibile_trees += 1;
+                if next_tree.height >= tree.height {
+                    break;
+                }
+                vertical_index -= 1;
+            }
+
+            // look down
+            vertical_index = tree.coords.y + 1;
+            horizontal_index = tree.coords.x;
+            while vertical_index != forrest.edge.y + 1 {
+                let next_tree =
+                    forrest.trees[vertical_index as usize][horizontal_index as usize].clone();
+                looking_down_visible_tress += 1;
+
+                if next_tree.height >= tree.height {
+                    break;
+                }
+
+                vertical_index += 1;
+            }
+
+            // look left
+            vertical_index = tree.coords.y;
+            horizontal_index = tree.coords.x - 1;
+            while horizontal_index != -1 {
+                looking_left_visible_trees += 1;
+                let next_tree =
+                    forrest.trees[vertical_index as usize][horizontal_index as usize].clone();
+
+                if next_tree.height >= tree.height {
+                    break;
+                }
+
+                horizontal_index -= 1;
+            }
+
+            // look right
+            vertical_index = tree.coords.y;
+            horizontal_index = tree.coords.x + 1;
+            while horizontal_index != forrest.edge.x + 1 {
+                looking_right_visible_trees += 1;
+                let next_tree =
+                    forrest.trees[vertical_index as usize][horizontal_index as usize].clone();
+
+                if next_tree.height >= tree.height {
+                    break;
+                }
+
+                horizontal_index += 1;
+            }
+
+            let visibilty_score = looking_up_visibile_trees
+                * looking_down_visible_tress
+                * looking_left_visible_trees
+                * looking_right_visible_trees;
+
+            if visibilty_score > highest_visibility {
+                highest_visibility = visibilty_score
+            }
+        }
+    }
+
+    println!("top visibility: {}", highest_visibility);
+}
+
+fn parse_forrest(file_contents: String) -> structs::Forrest {
+    let mut y = 0;
+    let mut forrest: Vec<Vec<structs::Tree>> = vec![];
+
+    let rows = file_contents
+        .trim_end_matches("\n")
+        .split("\n")
+        .collect::<Vec<&str>>();
+
+    for row in rows {
+        let mut x = 0;
+        let mut trees: Vec<structs::Tree> = vec![];
+
+        let cols = row.split("").collect::<Vec<&str>>();
+        let heights = cols
+            .iter()
+            .filter(|c| **c != "")
+            .map(|c| c.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        for height in heights {
+            trees.push(structs::Tree::new(height, y, x));
+            x += 1;
+        }
+        y += 1;
+
+        forrest.push(trees);
+    }
+    structs::Forrest::new(forrest)
+}
---
+++ day8/src/structs.rs
@@ -0,0 +1,100 @@
+#[derive(Debug, Clone)]
+pub struct Coordinates {
+    pub x: i64,
+    pub y: i64,
+}
+
+#[derive(Debug, Clone)]
+pub struct Tree {
+    pub height: i32,
+    pub coords: Coordinates,
+}
+
+#[derive(Debug, Clone)]
+pub struct Forrest {
+    pub trees: Vec<Vec<Tree>>,
+    pub edge: Coordinates,
+}
+
+impl Tree {
+    pub fn new(height: i32, row: i64, col: i64) -> Tree {
+        Tree {
+            height,
+            coords: Coordinates { x: col, y: row },
+        }
+    }
+}
+
+impl Forrest {
+    pub fn new(trees: Vec<Vec<Tree>>) -> Forrest {
+        let edge_x = trees.get(0).unwrap().len() as i64;
+        let edge_y = trees.len() as i64;
+        Forrest {
+            trees,
+            edge: Coordinates {
+                x: edge_x - 1, // zero-indexed
+                y: edge_y - 1, // zero-indexed
+            },
+        }
+    }
+
+    pub fn is_edge(&self, tree: Tree) -> bool {
+        tree.coords.x <= 0
+            || tree.coords.y <= 0
+            || tree.coords.x >= self.edge.x
+            || tree.coords.y >= self.edge.y
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::{Forrest, Tree};
+
+    #[test]
+    fn test_is_edge() {
+        // single tree
+        let tree = Tree::new(1, 0, 0);
+        let forrest = Forrest::new(vec![vec![tree.clone()]]);
+        assert!(forrest.is_edge(tree.clone()));
+
+        // 2x2 trees
+        let tree1a = Tree::new(1, 0, 0);
+        let tree1b = Tree::new(1, 0, 1);
+        let tree2a = Tree::new(1, 1, 0);
+        let tree2b = Tree::new(1, 1, 1);
+        let forrest = Forrest::new(vec![
+            vec![tree1a.clone(), tree1b.clone()],
+            vec![tree2a.clone(), tree2b.clone()],
+        ]);
+        assert!(forrest.is_edge(tree1a.clone()));
+        assert!(forrest.is_edge(tree1b.clone()));
+        assert!(forrest.is_edge(tree2a.clone()));
+        assert!(forrest.is_edge(tree2b.clone()));
+
+        // 3x3 trees
+        let tree1a = Tree::new(1, 0, 0);
+        let tree1b = Tree::new(1, 0, 1);
+        let tree1c = Tree::new(1, 0, 2);
+        let tree2a = Tree::new(1, 1, 0);
+        let tree2b = Tree::new(1, 1, 1);
+        let tree2c = Tree::new(1, 1, 2);
+        let tree3a = Tree::new(1, 2, 0);
+        let tree3b = Tree::new(1, 2, 1);
+        let tree3c = Tree::new(1, 2, 2);
+        let forrest = Forrest::new(vec![
+            vec![tree1a.clone(), tree1b.clone(), tree1c.clone()],
+            vec![tree2a.clone(), tree2b.clone(), tree2c.clone()],
+            vec![tree3a.clone(), tree3b.clone(), tree3c.clone()],
+        ]);
+        assert!(forrest.is_edge(tree1a.clone()));
+        assert!(forrest.is_edge(tree1b.clone()));
+        assert!(forrest.is_edge(tree1c.clone()));
+        assert!(forrest.is_edge(tree2a.clone()));
+        assert!(!forrest.is_edge(tree2b.clone()));
+        assert!(forrest.is_edge(tree2c.clone()));
+        assert!(forrest.is_edge(tree3a.clone()));
+        assert!(forrest.is_edge(tree3b.clone()));
+        assert!(forrest.is_edge(tree3c.clone()));
+    }
+}
