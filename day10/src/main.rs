40ac06f42c95008bdcf39eb7827865b2c2195471
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-19 22:57:02 +0000

    some refactoring

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -18,23 +18,57 @@ fn main() {
 
 fn p1(commands: Vec<&str>) {
     let mut register_x: i64 = 1;
-    let mut clock: u64 = 1;
+    let mut clock: i64 = 0;
+    let mut index = 0;
+    let mut signal_strength = 0;
 
-    for command in commands {
-        let command = command.split(" ").collect::<Vec<&str>>();
+    while index < commands.len() {
+        let command = commands[index].split(" ").collect::<Vec<&str>>();
         let instruction = command[0];
 
-        match instruction {
-            "addx" => {
-                let val = command[1].parse::<i64>().unwrap();
-                register_x += val;
-                clock += 2;
+        let cycles = match instruction {
+            "addx" => 2,
+            "noop" => 1,
+            _ => panic!("huh?"),
+        };
+
+        for _ in 0..cycles {
+            clock += 1;
+            match clock {
+                20 | 60 | 100 | 140 | 180 | 220 => signal_strength += register_x * clock,
+               _ => {}, 
             }
-            "noop" => clock += 1,
-            _ => panic!("huh"),
         }
+
+        if instruction == "addx" {
+            let val = command[1].parse::<i64>().unwrap();
+            register_x += val;
+        }
+
+        index += 1;
     }
 
-    println!("clock = {}", clock);
-    println!("register_x = {}", register_x);
+    println!("singal strength = {}", signal_strength);
+
+    // for command in commands {
+    //     let command = command.split(" ").collect::<Vec<&str>>();
+    //     let instruction = command[0];
+
+    //     match instruction {
+    //         "addx" => {
+    //             let val = command[1].parse::<i64>().unwrap();
+    //             register_x += val;
+    //             clock += 2;
+    //         }
+    //         "noop" => clock += 1,
+    //         _ => panic!("huh"),
+    //     }
+
+    //     if clock >= 220 {
+    //         break;
+    //     }
+    // }
+
+    // println!("clock = {}", clock);
+    // println!("register_x = {}", register_x);
 }
