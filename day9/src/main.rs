f348814e8af45f4c16d29ceec3cc59b97b4f8976
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-19 11:02:20 +0000

    solution to p2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 
     match part.as_str() {
         "p1" => p1(instructions),
+        "p2" => p2(instructions),
         _ => println!(""),
     }
 }
@@ -59,7 +60,67 @@ fn p1(instructions: Vec<&str>) {
         }
     }
 
-    println!("{}", tail_positions.len());
+    println!("total positions: {}", tail_positions.len());
+}
+
+fn p2(instructions: Vec<&str>) {
+    let mut tail_positions: HashSet<(i64, i64)> = HashSet::new();
+    let mut head: (i64, i64) = (0, 0);
+    let mut knots = vec![
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+    ];
+
+    tail_positions.insert(*knots.last().unwrap());
+
+    for instruction in instructions {
+        let instruction = instruction.split(" ").collect::<Vec<&str>>();
+        let direction = instruction[0];
+        let count = instruction[1].parse::<i64>().unwrap();
+
+        let (mut hx, mut hy) = head;
+
+        for _ in 0..count {
+            match direction {
+                "R" => hx += 1,
+                "L" => hx -= 1,
+                "U" => hy += 1,
+                "D" => hy -= 1,
+                _ => println!("huh?"),
+            }
+
+            head = (hx, hy);
+
+            let (mut prev_x, mut prev_y) = head;
+
+            for (index, knot) in knots.clone().iter().enumerate() {
+                let (mut kx, mut ky) = knot;
+                // println!("{:?} {:?}, {:?} {:?}", prev_x, prev_y, kx, ky);
+                if are_separated(prev_x, prev_y, kx, ky) {
+                    // println!("index = {:?}, prev_x = {:?}, prev_y = {:?}, kx = {:?}, ky = {:?}", index, prev_x, prev_y, kx, ky);
+                    let diff_x = prev_x - kx;
+                    let diff_y = prev_y - ky;
+                    kx += diff_x.signum();
+                    ky += diff_y.signum();
+
+                    knots.remove(index);
+                    knots.insert(index, (kx, ky));
+                }
+                (prev_x, prev_y) = (kx, ky);
+            }
+            tail_positions.insert(*knots.last().unwrap());
+        }
+        // println!("{:?} {:?}", head, knots);
+        // break;
+    }
+    println!("total positions: {}", tail_positions.len());
 }
 
 fn are_adjacent(hx: i64, hy: i64, tx: i64, ty: i64) -> bool {
@@ -68,9 +129,15 @@ fn are_adjacent(hx: i64, hy: i64, tx: i64, ty: i64) -> bool {
     diff_x < 2 || diff_y < 2
 }
 
+fn are_separated(hx: i64, hy: i64, tx: i64, ty: i64) -> bool {
+    let diff_x = (hx - tx).abs();
+    let diff_y = (hy - ty).abs();
+    diff_x > 1 || diff_y > 1
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::are_adjacent;
+    use crate::{are_adjacent, are_separated};
 
     #[test]
     fn test_are_adjacent() {
@@ -83,5 +150,10 @@ mod tests {
         assert!(are_adjacent(0, 0, 2, 1));
         assert!(!are_adjacent(0, 0, 2, 2));
         assert!(!are_adjacent(0, 0, -2, -2));
+    }
+
+    #[test]
+    fn test_are_separated() {
+        assert!(are_separated(2, 0, 0, 0))
     }
 }
