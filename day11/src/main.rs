12d57307576c12509c4f417f9a6a08059e3602de
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-29 19:54:28 +0000

    p2 solution bit it's slow

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,4 +1,5 @@
 pub mod structs;
+use num_bigint::{BigUint, ToBigUint};
 use std::{collections::HashMap, env, fs};
 
 fn main() {
@@ -10,6 +11,7 @@ fn main() {
 
     match part.as_str() {
         "p1" => p1(monkeys),
+        "p2" => p2(monkeys),
         _ => println!(""),
     }
 }
@@ -123,6 +125,118 @@ fn p1(monkeys: Vec<&str>) {
     println!("total monke biz = {}", total_monke_biz);
 }
 
+fn p2(monkeys: Vec<&str>) {
+    let mut monkes: Vec<structs::BigMonke> = Vec::new();
+    let mut monke_biz: HashMap<i64, i64> = HashMap::new();
+
+    for monkey in monkeys {
+        let monkey_array = monkey.split("\n").collect::<Vec<&str>>();
+        let monkey_items = monkey_array[1].trim().replace("Starting items: ", "");
+
+        let items = monkey_items
+            .split(",")
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|e| e.trim().parse::<i64>().unwrap())
+            .map(|e| e.to_biguint().unwrap())
+            .collect::<Vec<BigUint>>();
+
+        let op = monkey_array[2].trim().replace("Operation: new = ", "");
+        let divident = monkey_array[3]
+            .trim()
+            .replace("Test: divisible by ", "")
+            .parse::<i64>()
+            .unwrap()
+            .to_biguint()
+            .unwrap();
+        let dest_t = monkey_array[4]
+            .trim()
+            .replace("If true: throw to monkey ", "")
+            .parse::<i64>()
+            .unwrap();
+        let dest_f = monkey_array[5]
+            .trim()
+            .replace("If false: throw to monkey ", "")
+            .parse::<i64>()
+            .unwrap();
+        let monke = structs::BigMonke {
+            items,
+            divident,
+            dest_f,
+            dest_t,
+            op,
+        };
+
+        monkes.push(monke);
+    }
+
+    for _ in 0..1000 {
+        for monke_number in 0..monkes.len() {
+            // println!("Monke {}:", monke_number);
+            let monke = monkes[monke_number].clone();
+            for loop_index in 0..monke.items.len() {
+                let item = monke.items[loop_index].clone();
+                // print!("item = {}, ", item);
+                let worry_before_inspection = calc_worry_lvl_2(item.clone(), monke.op.clone());
+                let worry_after_inspection = worry_before_inspection.clone() / 3.to_biguint().unwrap();
+                let next_monke_index: usize;
+
+                if worry_after_inspection.clone() % monke.divident.clone() == 0.to_biguint().unwrap() {
+                    next_monke_index = usize::try_from(monke.dest_t).unwrap();
+                } else {
+                    next_monke_index = usize::try_from(monke.dest_f).unwrap();
+                }
+
+                // println!(
+                //     "give = {:?}, to monke = {:?}",
+                //     worry_after_inspection, next_monke_index
+                // );
+
+                monkes[next_monke_index].items.push(worry_after_inspection.clone());
+
+                let monke_number = i64::try_from(monke_number).unwrap();
+
+                if monke_biz.contains_key(&monke_number) {
+                    let existing_monke_biz = monke_biz.get(&monke_number).unwrap();
+                    monke_biz.insert(monke_number, existing_monke_biz + 1);
+                    continue;
+                }
+                monke_biz.insert(monke_number, 1);
+            }
+
+            monkes[monke_number].items.clear();
+
+            // println!();
+            // println!();
+        }
+    }
+
+    //     print!("monke {}: ", index);
+    //     for item in monke.items.clone() {
+    //         print!("{} ", item);
+    //     }
+    // println!();
+    // }
+
+    // println!();
+    // println!();
+
+    //     for (_monke, biz) in monke_biz.iter().enumerate() {
+    //         println!("monke {}, total biz: {:?}", biz.0, biz.1);
+    //     }
+
+    let mut l: Vec<_> = monke_biz.iter().map(|e| e.1).collect();
+    l.sort();
+    l.reverse();
+
+    //     println!();
+    //     println!("biz list = {:?}", l);
+
+    let total_monke_biz = l[0] * l[1];
+    println!();
+    println!("total monke biz = {}", total_monke_biz);
+}
+
 fn calc_worry_lvl(old_worry: i64, op: String) -> i64 {
     match op.as_str() {
         "old * 19" => old_worry * 19,
@@ -136,5 +250,23 @@ fn calc_worry_lvl(old_worry: i64, op: String) -> i64 {
         "old * 5" => old_worry * 5,
         "old + 2" => old_worry + 2,
         &_ => 0,
+    }
+}
+
+
+fn calc_worry_lvl_2(old_worry: BigUint, op: String) -> BigUint{
+    let f = old_worry.clone();
+    match op.as_str() {
+        "old * 19" => old_worry * 19.to_biguint().unwrap(),
+        "old + 6" => old_worry + 6.to_biguint().unwrap(),
+        "old * old" => old_worry * f,
+        "old + 3" => old_worry + 3.to_biguint().unwrap(),
+        "old * 13" => old_worry * 13.to_biguint().unwrap(),
+        "old + 7" => old_worry + 7.to_biguint().unwrap(),
+        "old + 5" => old_worry + 5.to_biguint().unwrap(),
+        "old + 8" => old_worry + 8.to_biguint().unwrap(),
+        "old * 5" => old_worry * 5.to_biguint().unwrap(),
+        "old + 2" => old_worry + 2.to_biguint().unwrap(),
+        &_ => 1.to_biguint().unwrap(),
     }
 }
