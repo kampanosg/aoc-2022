7fca73ec2c2663d9e1bbfc18ea4d821b6a715f4f
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-29 18:36:00 +0000

    p1 solution

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,5 +1,5 @@
 pub mod structs;
-use std::{env, fs};
+use std::{collections::HashMap, env, fs};
 
 fn main() {
     let file_path = env::args().nth(1).expect("param not provided: file_path");
@@ -16,6 +16,7 @@ fn main() {
 
 fn p1(monkeys: Vec<&str>) {
     let mut monkes: Vec<structs::Monke> = Vec::new();
+    let mut monke_biz: HashMap<i64, i64> = HashMap::new();
 
     for monkey in monkeys {
         let monkey_array = monkey.split("\n").collect::<Vec<&str>>();
@@ -55,57 +56,85 @@ fn p1(monkeys: Vec<&str>) {
         monkes.push(monke);
     }
 
-    for _ in 0..1 {
+    for _ in 0..20 {
         for monke_number in 0..monkes.len() {
-            println!("Monke {}:", monke_number);
+            // println!("Monke {}:", monke_number);
             let monke = monkes[monke_number].clone();
             for loop_index in 0..monke.items.len() {
                 let item = monke.items[loop_index];
-                print!("item = {}, ", item);
-                let worry_before_inspection =
-                    calc_worry_lvl(item.clone(), item.clone(), monke.op.clone());
+                // print!("item = {}, ", item);
+                let worry_before_inspection = calc_worry_lvl(item.clone(), monke.op.clone());
                 let worry_after_inspection = worry_before_inspection / 3;
-                let is_divisible = (worry_after_inspection % monke.divident) == 0;
                 let next_monke_index: usize;
 
-                if is_divisible {
-                    println!(
-                        "give = {:?}, to monke = {:?}",
-                        worry_after_inspection, monke.dest_t
-                    );
+                if worry_after_inspection % monke.divident == 0 {
                     next_monke_index = usize::try_from(monke.dest_t).unwrap();
                 } else {
-                    println!(
-                        "give = {:?}, to monke = {:?}",
-                        worry_after_inspection, monke.dest_f
-                    );
                     next_monke_index = usize::try_from(monke.dest_f).unwrap();
                 }
 
-                monkes[monke_number].items.remove(0);
+                // println!(
+                //     "give = {:?}, to monke = {:?}",
+                //     worry_after_inspection, next_monke_index
+                // );
+
                 monkes[next_monke_index].items.push(worry_after_inspection);
+
+                let monke_number = i64::try_from(monke_number).unwrap();
+
+                if monke_biz.contains_key(&monke_number) {
+                    let existing_monke_biz = monke_biz.get(&monke_number).unwrap();
+                    monke_biz.insert(monke_number, existing_monke_biz + 1);
+                    continue;
+                }
+                monke_biz.insert(monke_number, 1);
             }
 
-            println!();
-            println!();
+            monkes[monke_number].items.clear();
+
+            // println!();
+            // println!();
         }
     }
 
-    for (index, monke) in monkes.iter().enumerate() {
-        print!("monke {}: ", index);
-        for item in monke.items.clone() {
-            print!("{} ", item);
-        }
-        println!();
-    }
+    //     print!("monke {}: ", index);
+    //     for item in monke.items.clone() {
+    //         print!("{} ", item);
+    //     }
+    // println!();
+    // }
+
+    // println!();
+    // println!();
+
+    //     for (_monke, biz) in monke_biz.iter().enumerate() {
+    //         println!("monke {}, total biz: {:?}", biz.0, biz.1);
+    //     }
+
+    let mut l: Vec<_> = monke_biz.iter().map(|e| e.1).collect();
+    l.sort();
+    l.reverse();
+
+    //     println!();
+    //     println!("biz list = {:?}", l);
+
+    let total_monke_biz = l[0] * l[1];
+    println!();
+    println!("total monke biz = {}", total_monke_biz);
 }
 
-fn calc_worry_lvl(worry: i64, old_worry: i64, op: String) -> i64 {
+fn calc_worry_lvl(old_worry: i64, op: String) -> i64 {
     match op.as_str() {
-        "old * 19" => worry * 19,
-        "old + 6" => worry + 6,
+        "old * 19" => old_worry * 19,
+        "old + 6" => old_worry + 6,
         "old * old" => old_worry * old_worry,
         "old + 3" => old_worry + 3,
+        "old * 13" => old_worry * 13,
+        "old + 7" => old_worry + 7,
+        "old + 5" => old_worry + 5,
+        "old + 8" => old_worry + 8,
+        "old * 5" => old_worry * 5,
+        "old + 2" => old_worry + 2,
         &_ => 0,
     }
 }
