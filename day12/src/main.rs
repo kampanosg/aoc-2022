f8195641369185539bec8df316a5f23cf194f0b4
George Kampanos <george.kampanos@nandos.co.uk>
2022-12-30 19:14:04 +0000

    basic structs

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = env::args().nth(1).expect("param not provided: file_path");
+    let part = env::args().nth(2).expect("param not provided: path");
+
+    let file_contents = fs::read_to_string(file_path).unwrap();
+    // let monkeys = file_contents.split("\n\n").collect::<Vec<&str>>();
+
+    match part.as_str() {
+        "p1" => p1(),
+        _ => println!(""),
+    }
+}
+
+
+fn p1() {
+    todo!();
 }
---
+++ day12/structs.rs
@@ -0,0 +1,39 @@
+pub enum Hill {
+    Start(u8),
+    End(u8),
+    Hill(u8),
+}
+
+impl Hill {
+    fn height(&self) -> u8 {
+        match self {
+            Hill::Start(h) => *h,
+            Hill::End(h) => *h,
+            Hill::Hill(h) => *h,
+        }
+    }
+
+    fn can_reach(&self, other: &Hill) -> bool {
+        other.height().saturating_sub(self.height()) <= 1
+    }
+}
+
+impl From<char> for Hill {
+    fn from(value: char) -> Self {
+        match value {
+            'S' => Hill::Start(0),
+            'E' => Hill::End(25),
+            c if c.is_ascii_lowercase() => Hill::Hill(value as u8 - b'a'),
+            _ => unreachable!(),
+        }
+    }
+}
+
+type AdjacentHills = [Option<(usize, usize)>; 4];
+
+pub struct AreaMap {
+    pub hills: Vec<Vec<Hill>>,
+    pub graph: HashMap<(usize, usize), Neighbors>,
+    pub start_at: (usize, usize),
+    pub end_at: (usize, usize),
+}
