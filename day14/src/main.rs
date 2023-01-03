d77b6de3365be1da3aa94f92cda00abab5b8db45
George Kampanos <george.kampanos@nandos.co.uk>
2023-01-03 23:05:37 +0000

    read the file

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,3 +1,20 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+
+    let file_path = env::args().nth(1).expect("param not provided: file_path");
+    let part = env::args().nth(2).expect("param not provided: path");
+
+    let file_contents = fs::read_to_string(file_path).unwrap();
+
+    match part.as_str() {
+        "p1" => p1(),
+        _ => println!(""),
+    }
+
 }
+
+fn p1() {
+    
+}
+
