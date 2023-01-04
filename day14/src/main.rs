7732d75355917ad52065e8d4fb3c1cbc4205db2b
George Kampanos <george.kampanos@nandos.co.uk>
2023-01-04 00:30:07 +0000

    first pass on p1

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,12 +1,16 @@
 pub mod structs;
-use std::{env, fs};
+use std::{env, fs, collections::HashSet};
 
 fn main() {
     let file_path = env::args().nth(1).expect("param not provided: file_path");
     let part = env::args().nth(2).expect("param not provided: path");
 
     let file_contents = fs::read_to_string(file_path).unwrap();
-    let cave = parse_cave(&file_contents);
+    let parsed_cave = parse_cave(&file_contents);
+    let cave = structs::Cave {
+        rock_paths: parsed_cave,
+        sand: structs::Coord { x: 500, y: 0 },
+    };
 
     match part.as_str() {
         "p1" => p1(cave),
@@ -14,8 +18,42 @@ fn main() {
     }
 }
 
-fn p1(cave: Vec<structs::Path>) {
-    
+fn p1(cave: structs::Cave) {
+    let rock = structs::Rock::parse_from_rock_paths(&cave.rock_paths);
+    let lowest_y = *rock.hor.keys().max().unwrap();
+    let mut rested = HashSet::<structs::Coord>::new();
+    let mut sand_particle = cave.sand;
+
+    while sand_particle.y < lowest_y {
+        let possible_moves = [
+            structs::Coord {
+                x: sand_particle.x,
+                y: sand_particle.y + 1,
+            },
+            structs::Coord {
+                x: sand_particle.x - 1,
+                y: sand_particle.y + 1,
+            },
+            structs::Coord {
+                x: sand_particle.x + 1,
+                y: sand_particle.y + 1,
+            },
+        ];
+
+        let next_pos = possible_moves
+            .iter()
+            .find(|p| !rock.is_touching(p) && !rested.contains(p));
+
+        match next_pos {
+            Some(p) => sand_particle = *p,
+            None => {
+                rested.insert(sand_particle);
+                sand_particle = cave.sand;
+            }
+        }
+    }
+
+    println!("total = {}", rested.len());
 }
 
 fn parse_cave(input: &str) -> Vec<structs::Path> {
--- day14/src/structs.rs
+++ day14/src/structs.rs
@@ -1,4 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    cmp,
+    collections::{HashMap, HashSet},
+};
 
 pub type Path = Vec<Coord>;
 
@@ -27,13 +30,80 @@ pub struct CaveWall {
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
-pub struct Range {
-    pub start: i32,
-    pub end: i32,
-}
+pub struct Range(i32, i32);
 
 #[derive(Debug)]
 pub struct Rock {
     pub ver: HashMap<i32, HashSet<Range>>,
     pub hor: HashMap<i32, HashSet<Range>>,
+}
+
+impl Rock {
+    pub fn is_touching(self: &Self, coord: &Coord) -> bool {
+        let is_touching_h_wall = self.hor.get(&coord.y).map_or(false, |v| {
+            v.iter()
+                .any(|Range(sx, ex)| Rock::is_within(sx.clone(), coord.x, ex.clone()))
+        });
+
+        let is_touching_v_wall = self.ver.get(&coord.x).map_or(false, |v| {
+            v.iter()
+                .any(|Range(sy, ey)| Rock::is_within(sy.clone(), coord.y, ey.clone()))
+        });
+
+        is_touching_v_wall || is_touching_h_wall
+    }
+
+    fn is_within(a: i32, coord: i32, b: i32) -> bool {
+        let min = cmp::min(a, b);
+        let max = cmp::max(a, b);
+        min <= coord && coord <= max
+    }
+
+    pub fn parse_from_rock_paths(rock_paths: &Vec<Path>) -> Self {
+        let mut hor_wall = HashMap::<i32, HashSet<Range>>::new();
+        let mut ver_wall = HashMap::<i32, HashSet<Range>>::new();
+
+        for path in rock_paths {
+            for i in 0..(path.len() - 1) {
+                let curr_coord = path[i];
+                let next_coord = path[i + 1];
+
+                if curr_coord.x == next_coord.x {
+                    let range = if curr_coord.y > next_coord.y {
+                        Range(next_coord.y, curr_coord.y)
+                    } else {
+                        Range(curr_coord.y, next_coord.y)
+                    };
+                    ver_wall
+                        .entry(curr_coord.x)
+                        .and_modify(|rs| {
+                            rs.insert(range.clone());
+                        })
+                        .or_insert(HashSet::from([range.clone()]));
+                } else if curr_coord.y == next_coord.y {
+                    let range = if curr_coord.x > curr_coord.y {
+                        Range(next_coord.x, curr_coord.x)
+                    } else {
+                        Range(curr_coord.x, next_coord.x)
+                    };
+                    hor_wall
+                        .entry(curr_coord.y)
+                        .and_modify(|rs| {
+                            rs.insert(range.clone());
+                        })
+                        .or_insert(HashSet::from([range.clone()]));
+                } else {
+                    panic!(
+                        "Diagonal paths aren't supported {:?} -> {:?}",
+                        curr_coord, next_coord
+                    );
+                }
+            }
+        }
+
+        Rock {
+            ver: ver_wall,
+            hor: hor_wall,
+        }
+    }
 }
