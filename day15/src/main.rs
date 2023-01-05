e19d043f00f001983c1b6a91a0931aa45e624c0d
George Kampanos <george.kampanos@nandos.co.uk>
2023-01-05 22:52:38 +0000

    works for test input

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -7,25 +7,55 @@ fn main() {
 
     let file_contents = fs::read_to_string(file_path).unwrap();
     let sensors = parse_sensors(file_contents);
-    dbg!(sensors);
 
     match part.as_str() {
-        "p1" => p1(),
+        "p1" => p1(sensors),
         _ => println!(""),
     }
 }
 
-fn p1() {
-    
+fn p1(sensors: Vec<structs::Sensor>) {
+    let mut total = 0;
+    let min_x = -4;
+    let max_x = 26;
+    let y = 10;
+
+    for x in min_x..=max_x {
+        let point = structs::Coord { x, y };
+
+        if sensors.iter().any(|rec| rec.beacon == point) {
+            // already have a beacon there, not an impossible position
+        } else if sensors.iter().any(|rec| {
+            let radius = rec.position.manhattan_distance(rec.beacon);
+            rec.position.manhattan_distance(point) <= radius
+        }) {
+            // covered!
+            total += 1
+        }
+    }
+
+    println!("total beacons: {}", total)
 }
 
 fn parse_sensors(file_contents: String) -> Vec<structs::Sensor> {
-    let mut sensors: Vec<structs::Sensor> = vec![]; 
+    let mut sensors: Vec<structs::Sensor> = vec![];
     for line in file_contents.lines() {
         let line = line.split(" ").collect::<Vec<&str>>();
-        let sensor_x = line[2].replace("x=", "").replace(",", "").parse::<i64>().unwrap();
-        let sensor_y = line[3].replace("y=", "").replace(":", "").parse::<i64>().unwrap();
-        let beacon_x = line[8].replace("x=", "").replace(",", "").parse::<i64>().unwrap();
+        let sensor_x = line[2]
+            .replace("x=", "")
+            .replace(",", "")
+            .parse::<i64>()
+            .unwrap();
+        let sensor_y = line[3]
+            .replace("y=", "")
+            .replace(":", "")
+            .parse::<i64>()
+            .unwrap();
+        let beacon_x = line[8]
+            .replace("x=", "")
+            .replace(",", "")
+            .parse::<i64>()
+            .unwrap();
         let beacon_y = line[9].replace("y=", "").parse::<i64>().unwrap();
 
         println!("{}, {}, {}, {}", sensor_x, sensor_y, beacon_x, beacon_y);
--- day15/src/structs.rs
+++ day15/src/structs.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct Coord {
     pub x: i64,
     pub y: i64,
@@ -12,8 +12,21 @@ pub struct Sensor {
 
 impl Sensor {
     pub fn new(sensor_x: i64, sensor_y: i64, beacon_x: i64, beacon_y: i64) -> Sensor {
-        let position = Coord { x: sensor_x, y: sensor_y };
-        let beacon = Coord { x: beacon_x, y: beacon_y};
+        let position = Coord {
+            x: sensor_x,
+            y: sensor_y,
+        };
+        let beacon = Coord {
+            x: beacon_x,
+            y: beacon_y,
+        };
         Sensor { position, beacon }
+    }
+}
+
+impl Coord {
+    // As shown in https://en.wikipedia.org/wiki/Taxicab_geometry
+    pub fn manhattan_distance(self, other: Self) -> i64 {
+        (self.x.abs_diff(other.x) + self.y.abs_diff(other.y)) as i64
     }
 }
