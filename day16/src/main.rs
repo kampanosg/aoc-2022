821b15a810320ed3c3b62588fe497de0199b2fe7
George Kampanos <george.kampanos@nandos.co.uk>
2023-01-08 23:23:08 +0000

    attempt at p1 (slow.. but works?)

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,92 +1,165 @@
 pub mod structs;
-use std::{
-    collections::{hash_map::Entry, HashMap},
-    env, fs,
-};
+use std::{env, fs};
+
+use std::collections::{BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+use itertools::Itertools;
+
+#[derive(Debug)]
+pub struct Valve<'a> {
+    pub flow_rate: u32,
+    pub adjacent_valves: HashSet<&'a str>,
+}
 
 fn main() {
     let file_path = env::args().nth(1).expect("param not provided: file_path");
     let part = env::args().nth(2).expect("param not provided: path");
-
     let file_contents = fs::read_to_string(file_path).unwrap();
-    let valves = parse_valves(file_contents);
-    let mapped_valves = map_valves(valves.clone());
-    let connected_valves = connect_valves(structs::ValveId::new("AA"), mapped_valves.clone());
 
-    for (valve_id, tunnel) in connected_valves {
-        println!("We can get to {:?} using tunnel {:?}", valve_id, tunnel);
-    }
+    let valves = parse_valves(file_contents.as_str());
+    let valve_distances = min_distances(&valves);
 
     match part.as_str() {
-        "p1" => p1(valves.clone()),
+        "p1" => p1(&valves, &valve_distances),
         _ => println!(""),
     }
 }
 
-fn p1(valves: Vec<structs::Valve>) {}
+fn p1(map: &HashMap<&str, Valve>, dist_map: &HashMap<(&str, &str), u32>) { 
+    let open_valves: HashSet<_> = map
+        .iter()
+        .filter(|(_, valve)| valve.flow_rate > 0)
+        .map(|(&name, _)| name)
+        .collect();
 
-fn parse_valves(file_contents: String) -> Vec<structs::Valve> {
-    let mut valves: Vec<structs::Valve> = vec![];
+    let mut total_relieved_valves = 0;
+    let mut moves = VecDeque::new();
+    let mut visited_valves = HashSet::new();
+
+    moves.push_back(structs::State {
+        curr: "AA",
+        opened: BTreeSet::new(),
+        elapsed: 0,
+        relieved: 0,
+    });
+
+    visited_valves.insert((BTreeSet::new(), 0, 0));
+
+    while let Some(structs::State {opened, curr, elapsed, relieved}) = moves.pop_front()
+    {
+        if opened.len() == open_valves.len() || elapsed >= 30 {
+            let relieved_at_end = wait_for_boom(30, elapsed, relieved, &opened, &map);
+            total_relieved_valves = total_relieved_valves.max(relieved_at_end);
+            continue;
+        }
+
+        let closed_valves = open_valves.iter().filter(|name| !opened.contains(*name));
+
+        for next_valve in closed_valves {
+            let cost = dist_map[&(curr, *next_valve)] + 1;
+            let new_elapsed = elapsed + cost;
+
+            if new_elapsed >= 30 {
+                let relieved_at_end = wait_for_boom(30, elapsed, relieved, &opened, &map);
+                total_relieved_valves = total_relieved_valves.max(relieved_at_end);
+                continue;
+            }
+
+            let relieved_per_min: u32 = opened.iter().map(|name| &map[name].flow_rate).sum();
+            let new_relieved = relieved + (relieved_per_min * cost);
+
+            let mut new_opened = opened.clone();
+            new_opened.insert(next_valve);
+
+            if visited_valves.insert((new_opened.clone(), new_elapsed, new_relieved)) {
+                moves.push_back(structs::State {
+                    opened: new_opened,
+                    curr: next_valve,
+                    elapsed: new_elapsed,
+                    relieved: new_relieved,
+                });
+            }
+        }
+    }
+
+    dbg!(total_relieved_valves);
+}
+
+// Return min cost to go from one valve to the orher, using Dijkstra's algorithm
+fn calc_min_cost(from: &str, to: &str, map: &HashMap<&str, Valve>) -> u32 {
+    let mut priority_queue = BinaryHeap::new();
+    let mut seen = HashSet::new();
+
+    priority_queue.push(structs::Node { cost: 0, curr: from, });
+    seen.insert(from);
+
+    while let Some(structs::Node { cost, curr }) = priority_queue.pop() {
+        if curr == to {
+            return cost;
+        }
+
+        for neighbour in map[curr].adjacent_valves.iter() {
+            if seen.insert(neighbour) {
+                priority_queue.push(structs::Node {
+                    cost: cost + 1,
+                    curr: neighbour,
+                });
+            }
+        }
+    }
+
+    u32::MAX
+}
+
+fn min_distances<'a>(map: &'a HashMap<&str, Valve>) -> HashMap<(&'a str, &'a str), u32> {
+    map.iter()
+        .filter(|(_, valve)| valve.flow_rate > 0)
+        .map(|(&name, _)| name)
+        .tuple_combinations()
+        .fold(HashMap::new(), |mut acc, (name1, name2)| {
+            acc.entry(("AA", name1))
+                .or_insert_with(|| calc_min_cost("AA", name1, map));
+            acc.entry(("AA", name2))
+                .or_insert_with(|| calc_min_cost("AA", name2, map));
+
+            let dist = calc_min_cost(name1, name2, map);
+            acc.insert((name1, name2), dist);
+            acc.insert((name2, name1), dist);
+
+            acc
+        })
+}
+
+fn wait_for_boom(max_time: u32, elapsed: u32, relieved: u32, opened: &BTreeSet<&str>, map: &HashMap<&str, Valve>) -> u32 {
+    let time_left = max_time - elapsed;
+    let relieved_per_min: u32 = opened.iter().map(|name| &map[name].flow_rate).sum();
+    relieved + (relieved_per_min * time_left)
+}
+
+fn parse_valves(file_contents: &str) -> HashMap<&str, Valve> {
+    let mut valves: HashMap<&str, Valve> = HashMap::new();
     for line in file_contents.lines() {
         let parts = line.split(" ").collect::<Vec<&str>>();
         let name = parts[1];
         let flow_rate = parts[4]
             .replace("rate=", "")
             .replace(";", "")
-            .parse::<u64>()
+            .parse::<u32>()
             .unwrap();
-        let next_valves = parts[9..].to_vec();
-        let mut adjacent_valves: Vec<structs::ValveId> = vec![];
-        for valve in next_valves {
-            adjacent_valves.push(structs::ValveId::new(valve));
-        }
-        valves.push(structs::Valve {
-            name: structs::ValveId::new(name),
-            flow_rate,
-            adjacent_valves,
-        });
+
+        let adjacent_valves = parts[9..]
+            .to_vec()
+            .iter()
+            .map(|f| f.trim_end_matches(","))
+            .collect::<HashSet<&str>>();
+
+        valves.insert(
+            name,
+            Valve {
+                flow_rate,
+                adjacent_valves,
+            },
+        );
     }
     valves
-}
-
-pub fn map_valves(valves: Vec<structs::Valve>) -> HashMap<structs::ValveId, structs::Valve> {
-    let mut mv: HashMap<structs::ValveId, structs::Valve> = HashMap::new();
-    for valve in valves {
-        if mv.contains_key(&valve.name) {
-            continue;
-        }
-        mv.insert(valve.name, valve);
-    }
-    mv
-}
-
-pub fn connect_valves(
-    starting_valve: structs::ValveId,
-    valves: HashMap<structs::ValveId, structs::Valve>,
-) -> HashMap<structs::ValveId, structs::Tunnel> {
-
-    let mut current_valve: HashMap<structs::ValveId, structs::Tunnel> = Default::default();
-    current_valve.insert(starting_valve, vec![]);
-
-    let mut connections = current_valve.clone();
-
-    while !current_valve.is_empty() {
-        let mut next: HashMap<structs::ValveId, structs::Tunnel> = Default::default();
-        for (name, tunnel) in current_valve {
-            for link in valves[&name].adjacent_valves.iter().copied() {
-                if let Entry::Vacant(e) = connections.entry(link) {
-                    let conn_path: structs::Tunnel = tunnel
-                        .iter()
-                        .copied()
-                        .chain(std::iter::once((name, link)))
-                        .collect();
-                    e.insert(conn_path.clone());
-                    next.insert(link, conn_path);
-                }
-            }
-        }
-        current_valve = next;
-    }
-
-    connections
 }
--- day16/src/structs.rs
+++ day16/src/structs.rs
@@ -1,64 +1,31 @@
-use std::fmt;
 
-#[derive(Clone, Debug)]
-pub struct Valve {
-    pub name: ValveId,
-    pub flow_rate: u64,
-    pub adjacent_valves: Vec<ValveId>,
+use std::{
+    cmp::Ordering,
+    collections::BTreeSet,
+};
+
+#[derive(PartialEq, Eq)]
+pub struct Node<'a> {
+    pub cost: u32,
+    pub curr: &'a str,
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct ValveId {
-    ch1: u8,
-    ch2: u8,
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+pub struct State<'a> {
+    pub opened: BTreeSet<&'a str>,
+    pub curr: &'a str,
+    pub elapsed: u32,
+    pub relieved: u32,
 }
 
-pub type Tunnel = Vec<(ValveId, ValveId)>;
-
-impl ValveId {
-    pub fn new(s: &str) -> Self {
-        let b = s.as_bytes();
-        Self {
-            ch1: b[0],
-            ch2: b[1],
-        }
+impl<'a> Ord for Node<'a> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
     }
 }
 
-impl fmt::Debug for ValveId {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let c1 = self.ch1 as char;
-        let c2 = self.ch2 as char;
-        write!(f, "{}{}", c1, c2)
-    }
-}
-
-#[derive(PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct State {
-    pressure: u32,
-    opened: u64,
-    pos: [u16; 2],
-    time: [u32; 2],
-}
-
-impl State {
-    fn upper_bound(&self, best_valves: &[Vec<(usize, u32, u32)>]) -> u32 {
-        let [mut max_t, mut min_t] = self.time;
-        let mut opened = self.opened;
-        let mut bound = self.pressure;
-        'next_valve: loop {
-            for (i, min_dist, f) in &best_valves[max_t as usize] {
-                if opened & (1 << i) == 0 {
-                    max_t -= min_dist;
-                    bound += f * max_t as u32;
-                    if max_t < min_t {
-                        (min_t, max_t) = (max_t, min_t);
-                    }
-                    opened |= 1 << i;
-                    continue 'next_valve;
-                }
-            }
-            return bound;
-        }
+impl<'a> PartialOrd for Node<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
