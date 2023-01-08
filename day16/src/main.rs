e1d902304c4b043b36058a921a24d59bb67315ce
George Kampanos <george.kampanos@nandos.co.uk>
2023-01-08 19:12:32 +0000

    find connected valves

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,5 +1,8 @@
 pub mod structs;
-use std::{env, fs};
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    env, fs,
+};
 
 fn main() {
     let file_path = env::args().nth(1).expect("param not provided: file_path");
@@ -7,30 +10,83 @@ fn main() {
 
     let file_contents = fs::read_to_string(file_path).unwrap();
     let valves = parse_valves(file_contents);
+    let mapped_valves = map_valves(valves.clone());
+    let connected_valves = connect_valves(structs::ValveId::new("AA"), mapped_valves.clone());
+
+    for (valve_id, tunnel) in connected_valves {
+        println!("We can get to {:?} using tunnel {:?}", valve_id, tunnel);
+    }
 
     match part.as_str() {
-        "p1" => p1(valves),
+        "p1" => p1(valves.clone()),
         _ => println!(""),
     }
 }
 
-fn p1(valves: Vec<structs::Valve>) {
-
-}
+fn p1(valves: Vec<structs::Valve>) {}
 
 fn parse_valves(file_contents: String) -> Vec<structs::Valve> {
     let mut valves: Vec<structs::Valve> = vec![];
     for line in file_contents.lines() {
         let parts = line.split(" ").collect::<Vec<&str>>();
         let name = parts[1];
-        let flow_rate = parts[4].replace("rate=", "").replace(";", "").parse::<u64>().unwrap();
+        let flow_rate = parts[4]
+            .replace("rate=", "")
+            .replace(";", "")
+            .parse::<u64>()
+            .unwrap();
         let next_valves = parts[9..].to_vec();
-        let mut adjacent_valves: Vec<String> = vec![];
+        let mut adjacent_valves: Vec<structs::ValveId> = vec![];
         for valve in next_valves {
-            let s = String::from(valve);
-            adjacent_valves.push(s);
+            adjacent_valves.push(structs::ValveId::new(valve));
         }
-        valves.push(structs::Valve{ name: String::from(name), flow_rate, adjacent_valves});
+        valves.push(structs::Valve {
+            name: structs::ValveId::new(name),
+            flow_rate,
+            adjacent_valves,
+        });
     }
     valves
+}
+
+pub fn map_valves(valves: Vec<structs::Valve>) -> HashMap<structs::ValveId, structs::Valve> {
+    let mut mv: HashMap<structs::ValveId, structs::Valve> = HashMap::new();
+    for valve in valves {
+        if mv.contains_key(&valve.name) {
+            continue;
+        }
+        mv.insert(valve.name, valve);
+    }
+    mv
+}
+
+pub fn connect_valves(
+    starting_valve: structs::ValveId,
+    valves: HashMap<structs::ValveId, structs::Valve>,
+) -> HashMap<structs::ValveId, structs::Tunnel> {
+
+    let mut current_valve: HashMap<structs::ValveId, structs::Tunnel> = Default::default();
+    current_valve.insert(starting_valve, vec![]);
+
+    let mut connections = current_valve.clone();
+
+    while !current_valve.is_empty() {
+        let mut next: HashMap<structs::ValveId, structs::Tunnel> = Default::default();
+        for (name, tunnel) in current_valve {
+            for link in valves[&name].adjacent_valves.iter().copied() {
+                if let Entry::Vacant(e) = connections.entry(link) {
+                    let conn_path: structs::Tunnel = tunnel
+                        .iter()
+                        .copied()
+                        .chain(std::iter::once((name, link)))
+                        .collect();
+                    e.insert(conn_path.clone());
+                    next.insert(link, conn_path);
+                }
+            }
+        }
+        current_valve = next;
+    }
+
+    connections
 }
--- day16/src/structs.rs
+++ day16/src/structs.rs
@@ -1,6 +1,34 @@
+use std::fmt;
 
+#[derive(Clone, Debug)]
 pub struct Valve {
-    pub name: String,
+    pub name: ValveId,
     pub flow_rate: u64,
-    pub adjacent_valves: Vec<String>,
+    pub adjacent_valves: Vec<ValveId>,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ValveId {
+    ch1: u8,
+    ch2: u8,
+}
+
+pub type Tunnel = Vec<(ValveId, ValveId)>;
+
+impl ValveId {
+    pub fn new(s: &str) -> Self {
+        let b = s.as_bytes();
+        Self {
+            ch1: b[0],
+            ch2: b[1],
+        }
+    }
+}
+
+impl fmt::Debug for ValveId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let c1 = self.ch1 as char;
+        let c2 = self.ch2 as char;
+        write!(f, "{}{}", c1, c2)
+    }
 }
