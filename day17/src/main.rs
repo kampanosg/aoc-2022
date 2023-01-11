0cf7e6ec22655d4873c807f020bec44327e9e57b
George Kampanos <george.kampanos@nandos.co.uk>
2023-01-11 14:14:43 +0000

    parse input to wind pattern

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,3 +1,4 @@
+pub mod structs;
 use std::{env, fs};
 
 fn main() {
@@ -5,11 +6,26 @@ fn main() {
     let part = env::args().nth(2).expect("param not provided: path");
     let file_contents = fs::read_to_string(file_path).unwrap();
 
+    let wind_pattern = parse_wind(file_contents);
+
     match part.as_str() {
-        "p1" => p1(),
+        "p1" => p1(wind_pattern),
         _ => println!(""),
     }
 }
 
-fn p1() {
+fn p1(wind_pattern: Vec<structs::Wind>) {
+    dbg!(wind_pattern);
+}
+
+fn parse_wind(file_contents: String) -> Vec<structs::Wind> {
+    file_contents
+        .trim_end_matches("\n")
+        .chars()
+        .map(|c| match c {
+            '>' => structs::Wind::Right,
+            '<' => structs::Wind::Left,
+            _ => panic!("not valid wind input")
+        })
+        .collect::<Vec<structs::Wind>>()
 }
