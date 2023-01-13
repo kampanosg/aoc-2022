c6e1c4dde3f7cd5158940bd6dfa3384c3fae3013
George Kampanos <george.kampanos@nandos.co.uk>
2023-01-13 11:19:17 +0000

    finish p2 and helpers

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,5 +1,5 @@
 pub mod structs;
-use std::{env, fs};
+use std::{collections::HashSet, env, fs};
 
 fn main() {
     let file_path = env::args().nth(1).expect("param not provided: file_path");
@@ -10,6 +10,7 @@ fn main() {
 
     match part.as_str() {
         "p1" => p1(cubes),
+        "p2" => p2(cubes),
         _ => println!(""),
     }
 }
@@ -21,6 +22,60 @@ fn p1(cubes: Vec<structs::Coord3D>) {
         .filter(|c| !cubes.contains(c))
         .count();
     println!("total = {}", total);
+}
+
+fn p2(cubes: Vec<structs::Coord3D>) {
+    let cubes: HashSet<structs::Coord3D> = cubes.into_iter().collect();
+    let visible = get_visible_cubes(&cubes);
+
+    let total = cubes
+        .iter()
+        .flat_map(|coord| coord.adjacent_coords())
+        .filter(|coord| visible.contains(coord))
+        .count();
+    println!("total = {}", total);
+}
+
+fn get_cube_bounds(cubes: &HashSet<structs::Coord3D>) -> [structs::Coord3D; 2] {
+    cubes.iter().fold(
+        [structs::Coord3D::default(), structs::Coord3D::default()],
+        |[mut min, mut max], cube| {
+            min.x = min.x.min(cube.x);
+            min.y = min.y.min(cube.y);
+            min.z = min.z.min(cube.z);
+            max.x = max.x.max(cube.x);
+            max.y = max.y.max(cube.y);
+            max.z = max.z.max(cube.z);
+            [min, max]
+        },
+    )
+}
+
+fn get_visible_cubes(cubes: &HashSet<structs::Coord3D>) -> HashSet<structs::Coord3D> {
+    let bounds = get_cube_bounds(cubes);
+    let mut visible = HashSet::new();
+
+    let start = structs::Coord3D::default();
+    let mut stack = Vec::new();
+    let mut seen = HashSet::new();
+
+    stack.push(start);
+    seen.insert(start);
+
+    while let Some(coord) = stack.pop() {
+        for neighbour in coord.adjacent_coords() {
+            if cubes.contains(&neighbour) || !neighbour.is_within_bounds(&bounds) {
+                continue;
+            }
+
+            if seen.insert(neighbour) {
+                stack.push(neighbour);
+                visible.insert(neighbour);
+            }
+        }
+    }
+
+    visible
 }
 
 fn parse_cubes(file_contents: String) -> Vec<structs::Coord3D> {
