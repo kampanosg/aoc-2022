12a43737333d06e64a4ba53ac84fcfa8339a4216
George Kampanos <george.kampanos@nandos.co.uk>
2023-01-13 10:43:57 +0000

    find adjacent cubes

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -4,7 +4,7 @@ use std::{env, fs};
 fn main() {
     let file_path = env::args().nth(1).expect("param not provided: file_path");
     let part = env::args().nth(2).expect("param not provided: path");
-    let file_contents = fs::read_to_string(file_path).unwrap();    
+    let file_contents = fs::read_to_string(file_path).unwrap();
 
     let cubes = parse_cubes(file_contents);
 
@@ -15,18 +15,24 @@ fn main() {
 }
 
 fn p1(cubes: Vec<structs::Coord3D>) {
-
+    let total = cubes
+        .iter()
+        .flat_map(|c| c.adjacent_coords())
+        .filter(|c| !cubes.contains(c))
+        .count();
+    println!("total = {}", total);
 }
 
 fn parse_cubes(file_contents: String) -> Vec<structs::Coord3D> {
     file_contents
         .trim_end_matches("\n")
         .lines()
-        .map(|l| { 
+        .map(|l| {
             let coord = l.split(",").collect::<Vec<&str>>();
-            let x = coord[0].parse::<u32>().unwrap();
-            let y = coord[1].parse::<u32>().unwrap();
-            let z = coord[2].parse::<u32>().unwrap();
-            structs::Coord3D {x, y, z }
-        }).collect::<Vec<structs::Coord3D>>()
+            let x = coord[0].parse::<i32>().unwrap();
+            let y = coord[1].parse::<i32>().unwrap();
+            let z = coord[2].parse::<i32>().unwrap();
+            structs::Coord3D { x, y, z }
+        })
+        .collect::<Vec<structs::Coord3D>>()
 }
--- day18/src/structs.rs
+++ day18/src/structs.rs
@@ -1,6 +1,32 @@
-
+#[derive(Hash, PartialEq, Eq, Clone, Copy, Default)]
 pub struct Coord3D {
-    pub x: u32,
-    pub y: u32,
-    pub z: u32,
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
+}
+
+enum ThreeDimension {
+    X,
+    Y,
+    Z,
+}
+
+impl Coord3D {
+    pub fn adjacent_coords(&self) -> Vec<Coord3D> {
+        let mut adjacent = Vec::new();
+
+        for dimension in [ThreeDimension::X, ThreeDimension::Y, ThreeDimension::Z] {
+            for offset in [-1, 1] {
+                let mut adjacent_coord = self.clone();
+                match dimension {
+                    ThreeDimension::X => adjacent_coord.x += offset,
+                    ThreeDimension::Y => adjacent_coord.y += offset,
+                    ThreeDimension::Z => adjacent_coord.z += offset,
+                }
+                adjacent.push(adjacent_coord);
+            }
+        }
+
+        adjacent
+    }
 }
