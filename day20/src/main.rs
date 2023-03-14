36dd2fed0044d4b7e4987dbe01f0fc0000f8cce5
George Kampanos <gkabanos@icloud.com>
2023-03-14 19:35:28 +0000

    parse the file

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,3 +1,27 @@
+pub mod structs;
+use std::collections::VecDeque;
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = env::args().nth(1).expect("param not provided: file_path");
+    let part = env::args().nth(2).expect("param not provided: path");
+    let file_contents = fs::read_to_string(file_path).unwrap();
+
+    let numbers = parse_numbers(file_contents);
+
+    match part.as_str() {
+        "p1" => p1(numbers),
+        _ => println!(""),
+    }
+}
+
+fn p1(numbers: Vec<i64>) {
+    
+}
+
+fn parse_numbers(file_contents: String) -> Vec<i64> {
+    file_contents
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
 }
