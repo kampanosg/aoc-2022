96ab80b5bede92ff40ea0f4858de11e04e6df694
George Kampanos <gkabanos@icloud.com>
2023-03-27 20:35:08 +0100

    p1

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,5 +1,8 @@
 pub mod structs;
-use std::{collections::HashMap, env, fs};
+use std::{
+    collections::{BinaryHeap, HashMap, HashSet},
+    env, fs, iter,
+};
 
 fn main() {
     let file_path = env::args().nth(1).expect("param not provided: file_path");
@@ -14,8 +17,128 @@ fn main() {
     }
 }
 
-fn p1(map: (HashMap<structs::Coordinate, structs::Block>, usize, usize)) {
-    println!("p1");
+fn p1(mrc: (HashMap<structs::Coordinate, structs::Block>, usize, usize)) {
+    let (map, rows, cols) = mrc;
+    let walls: HashSet<structs::Coordinate> = map
+        .iter()
+        .filter(|(_, tile)| **tile == structs::Block::Wall)
+        .map(|(pos, _)| *pos)
+        .collect();
+
+    let least_common_mult = calc_least_common_multiple(rows - 2, cols - 2);
+    let wind_patterns = build_wind_patterns(&map, rows, cols, least_common_mult);
+    let start_position = structs::Coordinate { y: 0, x: 1 };
+    let end_position = structs::Coordinate {
+        y: rows - 1,
+        x: cols - 2,
+    };
+
+    let mut q = BinaryHeap::new();
+    let mut seen = HashSet::new();
+
+    q.push(structs::Node {
+        cost: 0,
+        pos: start_position,
+    });
+    seen.insert((start_position, 0));
+
+    while let Some(structs::Node { cost, pos }) = q.pop() {
+        if pos == end_position {
+            println!("res={}", cost);
+            return;
+        }
+
+        let new_cost = cost + 1;
+        let winds = &wind_patterns[&(new_cost % least_common_mult)];
+
+        let candidates = pos
+            .adjacent_coords(rows, cols)
+            .into_iter()
+            .chain(iter::once(pos))
+            .filter(|coord| !walls.contains(coord))
+            .filter(|coord| !winds.contains(coord));
+
+        for new_pos in candidates {
+            if seen.insert((new_pos, new_cost)) {
+                q.push(structs::Node {
+                    cost: new_cost,
+                    pos: new_pos,
+                });
+            }
+        }
+    }
+}
+
+fn build_wind_patterns(
+    map: &HashMap<structs::Coordinate, structs::Block>,
+    rows: usize,
+    cols: usize,
+    max_time: usize,
+) -> HashMap<usize, HashSet<structs::Coordinate>> {
+    let mut pattern = HashMap::new();
+
+    let mut winds: Vec<(structs::Coordinate, structs::Compass)> = map
+        .iter()
+        .filter_map(|(pos, tile)| match tile {
+            structs::Block::Wall => None,
+            structs::Block::Wind(dir) => Some((*pos, *dir)),
+        })
+        .collect();
+
+    let coords = winds.iter().map(|(coord, _)| *coord).collect();
+    pattern.insert(0, coords);
+
+    for t in 1..max_time {
+        for (coord, dir) in winds.iter_mut() {
+            *coord = coord.add_adjacent_coord(dir);
+            match dir {
+                structs::Compass::Left => {
+                    if coord.x == 0 {
+                        coord.x = cols - 2;
+                    }
+                }
+                structs::Compass::Right => {
+                    if coord.x == cols - 1 {
+                        coord.x = 1;
+                    }
+                }
+                structs::Compass::Up => {
+                    if coord.y == 0 {
+                        coord.y = rows - 2;
+                    }
+                }
+                structs::Compass::Down => {
+                    if coord.y == rows - 1 {
+                        coord.y = 1;
+                    }
+                }
+            }
+        }
+        let coords = winds.iter().map(|(coord, _)| *coord).collect();
+        pattern.insert(t, coords);
+    }
+    pattern
+}
+
+fn calc_least_common_multiple(first: usize, second: usize) -> usize {
+    first * second / cacl_greatest_common_divisor(first, second)
+}
+
+fn cacl_greatest_common_divisor(first: usize, second: usize) -> usize {
+    let mut max = first;
+    let mut min = second;
+    if min > max {
+        std::mem::swap(&mut max, &mut min);
+    }
+
+    loop {
+        let res = max % min;
+        if res == 0 {
+            return min;
+        }
+        max = min;
+        min = res;
+    }
 }
 
 fn parse_map(input: &str) -> (HashMap<structs::Coordinate, structs::Block>, usize, usize) {
--- day24/src/structs.rs
+++ day24/src/structs.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 pub struct Coordinate {
     pub x: usize,
@@ -16,4 +18,64 @@ pub enum Compass {
     Right,
     Down,
     Left,
+}
+
+#[derive(PartialEq, Eq)]
+pub struct Node {
+    pub cost: usize,
+    pub pos: Coordinate,
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Coordinate {
+    pub fn adjacent_coords(&self, rows: usize, cols: usize) -> Vec<Self> {
+        use Compass::*;
+        let mut adj = Vec::new();
+        if self.y > 0 {
+            adj.push(self.add_adjacent_coord(&Up));
+        }
+        if self.x < cols - 1 {
+            adj.push(self.add_adjacent_coord(&Right));
+        }
+        if self.y < rows - 1 {
+            adj.push(self.add_adjacent_coord(&Down));
+        }
+        if self.x > 0 {
+            adj.push(self.add_adjacent_coord(&Left));
+        }
+        adj
+    }
+
+    pub fn add_adjacent_coord(&self, dir: &Compass) -> Self {
+        use Compass::*;
+        match dir {
+            Up => Coordinate {
+                y: self.y - 1,
+                x: self.x,
+            },
+            Right => Coordinate {
+                y: self.y,
+                x: self.x + 1,
+            },
+            Down => Coordinate {
+                y: self.y + 1,
+                x: self.x,
+            },
+            Left => Coordinate {
+                y: self.y,
+                x: self.x - 1,
+            },
+        }
+    }
 }
