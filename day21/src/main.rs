7b6b6381235b089d48e58fe4493ff853f1984c51
George Kampanos <gkabanos@icloud.com>
2023-03-27 01:21:30 +0100

    project setup

---
+++ day21/src/main.rs
@@ -0,0 +1,18 @@
+use std::{env, fs};
+
+fn main() {
+    let file_path = env::args().nth(1).expect("param not provided: file_path");
+    let part = env::args().nth(2).expect("param not provided: path");
+    let file_contents = fs::read_to_string(file_path).unwrap();
+
+    let monkes = 1;
+
+    match part.as_str() {
+        "p1" => p1(),
+        _ => println!(""),
+    }
+}
+
+fn p1() {
+    println!("p1");
+}
