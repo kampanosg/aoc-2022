72c628a2b96f85f2f292c9eacd67eb10a66f8226
George Kampanos <gkabanos@icloud.com>
2023-03-27 02:17:27 +0100

    p2 + formatting

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
 
     match part.as_str() {
         "p1" => p1(moves),
+        "p2" => p2(moves),
         _ => println!(""),
     }
 }
@@ -85,6 +86,110 @@ fn wrap(
         };
     }
     curr
+}
+
+pub fn p2(input: (Vec<Vec<structs::Block>>, Vec<structs::Move>)) {
+    let (map, instructions) = input;
+    let start_col = map[0]
+        .iter()
+        .position(|tile| *tile == structs::Block::Available)
+        .unwrap() as i32;
+
+    let mut current_pos = structs::Coordinate {
+        row: 0,
+        col: start_col,
+    };
+    let mut dir = structs::Direction::R;
+
+    for inst in &instructions {
+        match inst {
+            structs::Move::Rotate(turn) => dir = dir.turn(turn),
+            structs::Move::Up(amount) => {
+                for _ in 0..*amount {
+                    let structs::Coordinate { row: dr, col: dc } = dir.offset();
+                    let new_tile = map
+                        .get((current_pos.row + dr) as usize)
+                        .and_then(|row| row.get((current_pos.col + dc) as usize))
+                        .unwrap_or(&structs::Block::Empty);
+
+                    match new_tile {
+                        structs::Block::Available => {
+                            current_pos = structs::Coordinate {
+                                row: current_pos.row + dr,
+                                col: current_pos.col + dc,
+                            };
+                        }
+                        structs::Block::Empty => {
+                            let (new_pos, new_dir) = turn_around(&current_pos, &dir);
+                            if map[new_pos.row as usize][new_pos.col as usize]
+                                == structs::Block::Wall
+                            {
+                                break;
+                            }
+                            current_pos = new_pos;
+                            dir = new_dir
+                        }
+                        structs::Block::Wall => break,
+                    }
+                }
+            }
+        }
+    }
+
+    let res = 1000 * (current_pos.row + 1) + 4 * (current_pos.col + 1) + dir.score() as i32;
+    println!("res: {}", res);
+}
+
+fn turn_around(
+    pos: &structs::Coordinate,
+    dir: &structs::Direction,
+) -> (structs::Coordinate, structs::Direction) {
+    let (cube_row, cube_col, new_dir) = match (pos.row / 50, pos.col / 50, dir) {
+        (0, 1, structs::Direction::U) => (3, 0, structs::Direction::R),
+        (0, 1, structs::Direction::L) => (2, 0, structs::Direction::R),
+        (0, 2, structs::Direction::U) => (3, 0, structs::Direction::U),
+        (0, 2, structs::Direction::R) => (2, 1, structs::Direction::L),
+        (0, 2, structs::Direction::D) => (1, 1, structs::Direction::L),
+        (1, 1, structs::Direction::R) => (0, 2, structs::Direction::U),
+        (1, 1, structs::Direction::L) => (2, 0, structs::Direction::D),
+        (2, 0, structs::Direction::U) => (1, 1, structs::Direction::R),
+        (2, 0, structs::Direction::L) => (0, 1, structs::Direction::R),
+        (2, 1, structs::Direction::R) => (0, 2, structs::Direction::L),
+        (2, 1, structs::Direction::D) => (3, 0, structs::Direction::L),
+        (3, 0, structs::Direction::R) => (2, 1, structs::Direction::U),
+        (3, 0, structs::Direction::D) => (0, 2, structs::Direction::D),
+        (3, 0, structs::Direction::L) => (0, 1, structs::Direction::D),
+        _ => unreachable!(),
+    };
+    let (row_idx, col_idx) = (pos.row % 50, pos.col % 50);
+
+    let i = match dir {
+        structs::Direction::L => 49 - row_idx,
+        structs::Direction::R => row_idx,
+        structs::Direction::U => col_idx,
+        structs::Direction::D => 49 - col_idx,
+    };
+
+    // find new idxes within the cube
+    let new_row = match new_dir {
+        structs::Direction::L => 49 - i,
+        structs::Direction::R => i,
+        structs::Direction::U => 49,
+        structs::Direction::D => 0,
+    };
+    let new_col = match new_dir {
+        structs::Direction::L => 49,
+        structs::Direction::R => 0,
+        structs::Direction::U => i,
+        structs::Direction::D => 49 - i,
+    };
+
+    let new_pos = structs::Coordinate {
+        row: cube_row * 50 + new_row,
+        col: cube_col * 50 + new_col,
+    };
+
+    (new_pos, new_dir)
 }
 
 fn parse_moves(input: &str) -> (Vec<Vec<structs::Block>>, Vec<structs::Move>) {
