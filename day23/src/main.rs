e1631521354303fd1be41c7cecbf256db59ae8f4
George Kampanos <gkabanos@icloud.com>
2023-03-27 20:01:17 +0100

    solution to p2

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -11,6 +11,7 @@ fn main() {
 
     match part.as_str() {
         "p1" => p1(elves),
+        "p2" => p2(elves),
         _ => println!("invalid part param (valid options: p1 | p2)"),
     }
 }
@@ -77,6 +78,60 @@ fn p1(e: HashSet<structs::Coordinate>) {
                 println!("res: {}", res);
             }
     }
+}
+
+pub fn p2(e: HashSet<structs::Coordinate>) {
+    let mut elves = e.clone();
+    let mut checks = [
+        structs::Compass::North,
+        structs::Compass::South,
+        structs::Compass::West,
+        structs::Compass::East,
+    ];
+
+    for round in 1.. {
+        let mut moved = false;
+        // key: proposed coordinate, val: list of old coordinates that proposed going there
+        let mut proposals: HashMap<structs::Coordinate, Vec<structs::Coordinate>> = HashMap::new();
+
+        // consideration phase
+        for elf in &elves {
+            let neighbours = elf.adjacent_coord();
+            // If no other Elves are in one of those eight positions, the Elf does not do anything during this round.
+            if neighbours.iter().all(|coord| !elves.contains(coord)) {
+                continue;
+            }
+            let neighbours = neighbours
+                .iter()
+                .map(|neighbour| elves.contains(neighbour))
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
+
+            let proposed_dir = checks.iter().find(|dir| dir.check(&neighbours));
+            if let Some(dir) = proposed_dir {
+                let proposal = elf.add_coord(dir);
+                proposals.entry(proposal).or_default().push(*elf);
+            }
+        }
+
+        // movement phase
+        for (new_coord, old_coords) in proposals {
+            if old_coords.len() == 1 {
+                moved = true;
+                elves.remove(&old_coords[0]);
+                elves.insert(new_coord);
+            }
+        }
+
+        // after round
+        if !moved {
+            println!("res: {}", round);
+            return
+        }
+        checks.rotate_left(1);
+    }
+
 }
 
 fn parse_elves(input: &str) -> HashSet<structs::Coordinate> {
