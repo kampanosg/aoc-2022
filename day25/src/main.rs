776487af393e75d40a88f6beb7c5fcbe090f2c98
George Kampanos <gkabanos@icloud.com>
2023-03-28 04:39:00 +0100

    solution to p1

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -1,4 +1,3 @@
-pub mod structs;
 use std::{env, fs};
 
 fn main() {
@@ -7,13 +6,39 @@ fn main() {
 
     let file_contents = fs::read_to_string(file_path).unwrap();
 
-
     match part.as_str() {
-        "p1" => p1(),
+        "p1" => p1(&file_contents),
         _ => println!(""),
     }
 }
 
-fn p1() {
-    println!("p1");
+fn p1(input: &str) {
+    let sum = input.lines().map(from_str_to_decimal).sum();
+    let res = to_code(sum);
+    println!("res = {}", res);
+}
+
+fn from_str_to_decimal(input: &str) -> i64 {
+    input.chars().fold(0, |decimal, snafu_digit| {
+        let decimal_digit = ['=', '-', '0', '1', '2']
+            .into_iter()
+            .position(|c| c == snafu_digit)
+            .unwrap() as i64
+            - 2;
+        decimal * 5 + decimal_digit
+    })
+}
+
+fn to_code(d: i64) -> String {
+    if d == 0 {
+        return String::new();
+    }
+
+    let next_decimal = (d + 2) / 5;
+    let mut code = to_code(next_decimal);
+
+    let rem = d % 5;
+    let code_char = ['0', '1', '2', '=', '-'][rem as usize];
+    code.push(code_char);
+    code
 }
